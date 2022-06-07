7053b32ca815545f76f3e36a0100f8c3bee5bd84
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-07 23:01:09 +0200

    fekk2

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,14 @@ const Rokstar = ({Component, pageProps}) => {
     return (
         <>
             <Head>
-                <title>Rokstar :: React Portfolio Template</title>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-224266938-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}{'}'}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-224266938-1');
+</script>
             </Head>
             <Component {...pageProps} />
         </>
