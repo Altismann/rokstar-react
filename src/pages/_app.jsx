12c4b42e2da79de584bbc7dea73fc858b7eed01a
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-07 23:04:58 +0200

    fekk4

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,14 +4,7 @@ const Rokstar = ({Component, pageProps}) => {
     return (
         <>
             <Head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-224266938-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}{'}'}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-224266938-1');
-</script>
+                
             </Head>
             <Component {...pageProps} />
         </>
