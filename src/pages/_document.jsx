78a074a32ca4327bf914418b5e7649073751fb7b
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-07 22:58:23 +0200

    fekk

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -10,6 +10,15 @@ class MyDocument extends Document {
         return (
             <Html lang="sv">
                 <Head>
+                
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-224266938-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}{'}'}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-224266938-1');
+</script>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Russo+One&display=swap" rel="stylesheet"/>
