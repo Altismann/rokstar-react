c917f6197bb59d254e680b3d3d2d8a3dbc0baf2d
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-07 15:17:19 +0200

    hutt

--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -14,9 +14,10 @@ const HeroArea = () => {
                     <h1 className="text-white font-light text-3xl sm:text-[45px] md:text-[55px] leading-none">SITECRAFT</h1>
                     <h2 className="text-white font-light text-md sm:text-lg tracking-4 mt-2 sm:mt-4">Enklare Hemsidor</h2>
                     <p className="text-white font-light text-3xs ss:text-[11px] sm:text-[12px] md:text-[13px] tracking-1 leading-4">
-                    Enklare hemsidor är tillverkade för att vara snabba och minimalistiska i sitt utseende och för att stödja viktiga funktioner och sökoptimeringstekniker (SEO) 
-                    Sidorna byggs med Wordpress och NextJS och vi använder oss av nya och stabila hostinglösningar. Då vi endast gör sidor som hobbyverksamhet kostar en färdig sida 
-                    mellan 2000-4000kr och vi har ingen support eller egen lagring av data.
+                    Sitecraft är en webbyrå som skapar hemsidor är skapade för att vara snabba och minimalistiska i sitt
+                    utseende och för att stödja viktiga funktioner och sökoptimeringstekniker (SEO) Sidorna byggs
+                    med Wordpress och NextJS och vi använder oss av nya och stabila hostinglösningar. Då vi endast gör sidor 
+                    som hobbyverksamhet kostar en färdig sida mellan 2000-4000kr och vi har ingen support eller egen lagring av data.
                     </p>
                     <nav className="flex-col flex-grow">
 
