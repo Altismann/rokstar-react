621ce45698c555194fe81099bd6a947f38dc3534
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-07 23:11:32 +0200

    fikk

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,22 @@ import '../styles/globals.css';
 const Rokstar = ({Component, pageProps}) => {
     return (
         <>
+
+<Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  />
+
+  <Script id="google-analytics" strategy="lazyOnload">
+    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+  </Script>
             <Head>
                 
             </Head>
