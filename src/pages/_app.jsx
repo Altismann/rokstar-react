f3698ab2b74dc62d627d3a9ba65ecd0f015cce9d
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-08 10:22:59 +0200

    fiol

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,33 +1,15 @@
-import Script from 'next/script';
+import Head from "next/head";
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
- 
-
-    //...
-      return (
+const Rokstar = ({Component, pageProps}) => {
+    return (
         <>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
-          />
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-MZEMGBYQZR', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-    
-          <Layout>
+            <Head>
+        
+            </Head>
             <Component {...pageProps} />
-          </Layout>
         </>
-      );
-    }
+    )
+}
+
+export default Rokstar
