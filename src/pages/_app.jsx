27cb68dc3b37dbc8e3953dd4bf529dafe8d6dc62
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-20 00:27:04 +0200

    jhg

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,17 +6,9 @@ const Rokstar = ({Component, pageProps}) => {
     return (
         <>
             <Head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MZEMGBYQZR');
-</script>
             <Script
     strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    src={"https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR"}
   />
 
   <Script id="google-analytics" strategy="lazyOnload">
@@ -24,7 +16,7 @@ const Rokstar = ({Component, pageProps}) => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'G-MZEMGBYQZR');
               page_path: window.location.pathname,
             });
                 `}
