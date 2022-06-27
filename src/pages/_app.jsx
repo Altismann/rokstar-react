a2fed6d295410221aec866b59362814d44923c15
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-27 23:12:56 +0200

    bra

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,18 +8,16 @@ const Rokstar = ({Component, pageProps}) => {
             <Head>
             <Script
     strategy="lazyOnload"
-    src={"https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR"}
+    src={"https://www.googletagmanager.com/gtag/js?id=UA-224266938-1"}
   />
 
   <Script id="google-analytics" strategy="lazyOnload">
-    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-MZEMGBYQZR');
-              page_path: window.location.pathname,
-            });
-                `}
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-224266938-1');
+  `}
   </Script>
             </Head>
             <Component {...pageProps} />
