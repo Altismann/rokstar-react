0e1c9ffa1862f2d4972d08b2a1e4ddbcad48a04f
Altismann <87611738+Altismann@users.noreply.github.com>
2022-06-28 16:50:49 +0200

    hh

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,23 +4,14 @@ import Script from "next/script";
 const Rokstar = ({Component, pageProps}) => {
     return (
         <>
-          
-            <Script
-    strategy="lazyOnload"
-    src={"https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR"}
-  />
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR">
 
-  <Script id="google-analytics" strategy="lazyOnload">
-    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-MZEMGBYQZR');
-              page_path: window.location.pathname,
-            });
-                `}
-  </Script>
-            
+  window.dataLayer = window.dataLayer || [];
+  function gtag()dataLayer.push(arguments)
+  gtag('js', new Date());
+
+  gtag('config', 'G-MZEMGBYQZR');
+</script>
             <Component {...pageProps} />
         </>
     )
