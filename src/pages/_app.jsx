68a6467995e59aa119894e2fec903b1fa1626f24
Altismann <87611738+Altismann@users.noreply.github.com>
2022-07-02 21:00:15 +0200

    borta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,11 @@
+import Head from "next/head";
 import '../styles/globals.css';
-import Script from "next/script";
-
 const Rokstar = ({Component, pageProps}) => {
     return (
         <>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MZEMGBYQZR">
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag()dataLayer.push(arguments)
-  gtag('js', new Date());
-
-  gtag('config', 'G-MZEMGBYQZR');
-</script>
+            <Head>
+                <title>Rokstar :: React Portfolio Template</title>
+            </Head>
             <Component {...pageProps} />
         </>
     )
